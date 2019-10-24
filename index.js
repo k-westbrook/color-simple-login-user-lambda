e819ea68cede596956cb3bf34d0d1afe2af16fe7
k-westbrook <kcheriewestbrook@gmail.com>
2019-10-24 16:40:42 -0700

    updated totest

---
+++ index.js
@@ -0,0 +1,55 @@
+
+const AWS = require('aws-sdk');
+const docClient = new AWS.DynamoDB.DocumentClient({ region: "us-west-1" });
+
+exports.handler = (event, context, callback) => {
+  const params = {
+    Item: {
+      email: event.email,
+      password: event.password
+    },
+    TableName: "USER_INFO"
+  };
+
+  docClient.get(params, function (err, data) {
+
+    if (err) {
+
+      data = {
+        statusCode: 400,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true,
+        },
+        body: {
+          message: JSON.stringify('ERROR, email and/or password not allowed.')
+        }
+      };
+      //callback(null, data);
+
+    } else {
+
+      data = {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true,
+        },
+        body: {
+
+          message: JSON.stringify('Successful get.'),
+          user: params.Item
+        }
+
+      };
+
+      console.log(data, "SUCCESS")
+      // callback(null, data);
+
+    }
+  })
+
+
+
+
+};
